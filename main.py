85754132cc959da5b26a25e076cf20439f115ca4
WolfVD <57068085+WolfVD@users.noreply.github.com>
2020-06-14 23:41:55 -0400

    quit button

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ pygame.init()
 
 running = True
 
-fonts = pygame.font.get_fonts()
-print(len(fonts))
-for f in fonts:
-    print(f)
-
-screen = pygame.display.set_mode((1920,1080))
+screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
 pygame.display.set_caption('KeyboardLess') 
 screen.fill((255,255,255))
 
 basicFont = pygame.font.SysFont(None, 100)
 
 quitText = basicFont.render('QUIT', True, (0, 0, 0))
-screen.blit(quitText, (900,850))
+
+quitX = 875
+quitY = 850
+screen.blit(quitText, (quitX,quitY))
+quitRect = quitText.get_rect()
+quitRect.x = quitX
+quitRect.y = quitY
+
 
 while running:
     for event in pygame.event.get():
@@ -24,7 +26,8 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mousePos = pygame.mouse.get_pos()
-            print(mousePos)
+            if quitRect.collidepoint(mousePos):
+                running = False
 
     pygame.display.update()
 
