053944813e3a6ad55425e6fb0e5ec71366e86f8f
WolfVD <57068085+WolfVD@users.noreply.github.com>
2020-06-15 20:31:21 -0400

    Added words file

--- main.py
+++ main.py
@@ -3,22 +3,38 @@ import pygame.freetype
 pygame.init()
 
 running = True
+menuSetup = True
+menu = True
+typing = False
 
 screen = pygame.display.set_mode((1920,1080), pygame.FULLSCREEN)
 pygame.display.set_caption('KeyboardLess') 
-screen.fill((255,255,255))
 
-basicFont = pygame.font.SysFont(None, 100)
+Fboldsmall = pygame.font.Font("Raleway-Bold.ttf", 100)
+Fboldbig = pygame.font.Font("Raleway-Bold.ttf", 180)
 
-quitText = basicFont.render('QUIT', True, (0, 0, 0))
-
-quitX = 875
-quitY = 850
-screen.blit(quitText, (quitX,quitY))
+#region menu_text
+quitText = Fboldsmall.render('QUIT', True, (255, 255, 255))
+quitX = 960
+quitY = 900
 quitRect = quitText.get_rect()
-quitRect.x = quitX
-quitRect.y = quitY
+quitRect.x = quitX-(quitRect.width/2)
+quitRect.y = quitY-(quitRect.height/2)
 
+startText = Fboldbig.render('START', True, (255, 255, 255))
+startX = 960
+startY = 540
+startRect = startText.get_rect()
+startRect.x = startX-(startRect.width/2)
+startRect.y = startY-(startRect.height/2)
+#endregion menu_text
+
+#region typing_text
+
+
+#endregion typing_text
+
+words = open("words.txt", "r")
 
 while running:
     for event in pygame.event.get():
@@ -26,9 +42,24 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mousePos = pygame.mouse.get_pos()
-            if quitRect.collidepoint(mousePos):
+            if quitRect.collidepoint(mousePos) and menu == True:
                 running = False
+            if startRect.collidepoint(mousePos) and menu == True:
+                typing = True
 
-    pygame.display.update()
+    if typing == True:
+        screen.fill((0,0,0))
+        pygame.display.update()
+
+    if menuSetup == True:
+        screen.fill((0,0,0))
+        screen.blit(quitText, (quitX-(quitRect.width/2),quitY-(quitRect.height/2)))
+        screen.blit(startText, (startX-(startRect.width/2),startY-(startRect.height/2)))
+
+        menuSetup = False
+        menu = True
+
+        pygame.display.update()
+
 
 pygame.quit()
